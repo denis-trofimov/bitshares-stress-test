b6a2900652ee3854c3c2a57e6cf2a97873b6cf2f
denis_trofimov <silaradost@yandex.ru>
2018-10-01 18:52:56 +0300

    Add tcpkali test commands.

--- websocket_test_tcpkali.sh
+++ websocket_test_tcpkali.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
-tcpkali -vvv -c1 --channel-lifetime=0.1 -r1 --ws -m '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' hawking.array.io:8090
+tcpkali -T 1 -vvv -c1 --channel-lifetime=0.1 -r1 --ws -m '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' hawking.array.io:8090
 tcpkali -vvv -r1 --ws -m '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' -m '{"method": "call", "params": [1, "database", []], "id": 3}' -m '{"method": "call", "params": [2, "get_objects", [["2.1.0"]]], "id": 7}' hawking.array.io:8090
 tcpkali -vvv -r1 --ws -m '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' -m '{"method": "call", "params": [1, "database", []], "id": 3}' -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' hawking.array.io:8090
 tcpkali -vvv -c12 --connect-timeout 3 --latency-marker "last_irreversible_block_num" --message-stop "Assert Exception" -r @100ms --ws -m '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' -m '{"method": "call", "params": [1, "database", []], "id": 3}' -m '{"method": "call", "params": [2, "get_objects", [["2.1.0"]]], "id": 7}' hayking.array.io:8090
 tcpkali -vvv -c12 --connect-timeout 3 --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" -r @100ms --ws -m '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' -m '{"method": "call", "params": [1, "database", []], "id": 3}' -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' newton.array.io:8090
 
- tcpkali -vvv -c12 --connect-timeout 3 --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" -r @200ms --ws -m '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' -m '{"method": "call", "params": [1, "database", []], "id": 3}' -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' newton.array.io:8090
+tcpkali -vvv -c12 --connect-timeout 3 --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" -r @200ms --ws -m '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' -m '{"method": "call", "params": [1, "database", []], "id": 3}' -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' newton.array.io:8090
 ##[Mean    =       88.466, StdDeviation   =       73.909]
 ##[Max     =      612.300, Total count    =          498]
 ##[Buckets =           10, SubBuckets     =         2048]
@@ -18,3 +18,60 @@ tcpkali -vvv -c12 --connect-timeout 3 --latency-marker "transaction_merkle_root"
 #Message latency at percentiles: 171.0/471.5/521.1 ms (95/99/99.5%)
 #Test duration: 132.681 s.
 #Best --message-rate for latency 200ms⁹⁵ᵖ is 24.4385
+
+
+tcpkali -v 3 -c 10 -r1 --ws -1 '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' -1 '{"method": "call", "params": [1, "database", []], "id": 3}' -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' hawking.array.io:8090
+
+tcpkali -T 5 -vvv -c1 -r10 --ws --first-message-file first-message.json -f message_get_block.txt hawking.array.io:8090
+tcpkali -d -T 1 -vvv -c1 -r5 --ws -1 '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' -m '{"method": "call", "params": [1, "database", []], "id": 3}' -f message_get_block.txt hawking.array.io:8090
+tcpkali -d -T 1 -vvv -c1 -r5 --ws \
+-1 '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' \
+ -m '{"method": "call", "params": [1, "database", []], "id": 3}' \
+ -m '{"method": "call", "params": [2, "get_block", [\{re [0-9]{1,4}}]], "id": 7}' \
+ hawking.array.io:8090
+
+tcpkali -d -T 1 -vvv -c10 -r5 --ws --first-message-file first-message.json \
+-f message_get_block.txt hawking.array.io:8090
+
+tcpkali -d -T 1 -vvv -c1 -r100 --ws \
+-1 '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' \
+ -m '{"method": "call", "params": [1, "database", []], "id": 3}' \
+ -m '{"method": "call", "params": [2, "get_block", [\{re [0-9]{1,4}}]], "id": 7}' \
+ hawking.array.io:8090
+
+ tcpkali --ws -d -T 1 -vvv -c15 -r @200ms \
+--latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
+-1 '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' \
+ -m '{"method": "call", "params": [1, "database", []], "id": 3}' \
+ -m '{"method": "call", "params": [2, "get_block", [\{re [0-9]{1,4}}]], "id": 7}' \
+ hawking.array.io:8090
+
+tcpkali --ws -d -T 5 -vvv -c15 -r@500ms \
+--latency-marker "timestamp" --message-stop "Assert Exception" \
+-1 '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' \
+ -m '{"method": "call", "params": [1, "database", []], "id": 3}' \
+ -m '{"method": "call", "params": [2, "get_block", [\{re [0-9]{1,4}}]], "id": 7}' \
+ hawking.array.io:8090
+
+tcpkali -T 5 -vvv -c10 -r200 --ws \
+-1 '{"method": "call", "params": [1, "login", ["", ""]], "id": 2}' \
+ -m '{"method": "call", "params": [1, "database", []], "id": 3}' \
+ -m '{"method": "call", "params": [2, "get_block", [\{re [0-9]{1,4}}]], "id": 7}' \
+ hawking.array.io:8090
+
+tcpkali -vvv -c12 --connect-timeout 3 --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" -r @200ms --ws \
+-1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
+ -m '{"method": "call", "params": [2, "get_block", [\{re [0-9]{1,4}}]], "id": 7}' \
+ hawking.array.io:8090
+
+tcpkali -vvv --ws -c12 -r @200ms --connect-timeout 3 \
+ --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
+ -1 '{"method": "call", "params": [1, "database", []], "id": 3}'\
+ -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}'\
+ newton.array.io:8090
+
+ # Valid!
+tcpkali -d -T 1 -vvv -c1 -r10 --ws \
+-1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
+ -m '{"method": "call", "params": [2, "get_block", [\{re [0-9]{1,4}}]], "id": 7}' \
+ hawking.array.io:8090
