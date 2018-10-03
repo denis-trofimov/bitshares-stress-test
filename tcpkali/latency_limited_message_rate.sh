16326bda21a567470d832273621a9d0a4c79c43c
Denis Trofimov <silaradost@yandex.ru>
2018-10-03 21:49:05 +0300

    Make latency measurement less verbose.

--- tcpkali/latency_limited_message_rate.sh
+++ tcpkali/latency_limited_message_rate.sh
@@ -1,47 +1,47 @@
 #!/usr/bin/env bash #lends you some flexibility on different systems
-tcpkali -vvv --ws -c12 -r @500ms --connect-timeout 3 \
+tcpkali -vv --ws -c12 -r @500ms --connect-timeout 3 \
 --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
 -1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
 -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
 newton.array.io:8090  2>&1 | tee latency_500ms.log
 
-tcpkali -vvv --ws -c100 -r @1000ms --connect-rate 10 \
+tcpkali -vv --ws -c100 -r @1000ms --connect-rate 10 \
 --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
 -1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
 -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
 newton.array.io:8090  2>&1 | tee latency_r@1000ms_c100.log
 
-tcpkali -vvv --ws -c10 -r @1000ms \
+tcpkali -vv --ws -c10 -r @1000ms \
 --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
 -1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
 -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
 newton.array.io:8090  2>&1 | tee latency_r@1000ms_c10_gpon.log
 
-tcpkali -vvv --ws -c2 -r @1000ms \
+tcpkali -vv --ws -c2 -r @1000ms \
 --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
 -1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
 -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
 newton.array.io:8090  2>&1 | tee latency_r@1000ms_c2_gpon.log
 
-tcpkali -vvv --ws -c6 -r @1000ms \
+tcpkali -vv --ws -c6 -r @1000ms \
 --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
 -1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
 -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
 newton.array.io:8090  2>&1 | tee latency_r@1000ms_c6_gpon.log
 
-tcpkali -vvv --ws -c24 -r @1000ms \
+tcpkali -vv --ws -c24 -r @1000ms \
 --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
 -1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
 -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
 newton.array.io:8090  2>&1 | tee latency_r@1000ms_c24_gpon.log
 
-tcpkali -vvv --ws -c12 -r @1000ms \
+tcpkali -vv --ws -c12 -r @1000ms \
 --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
 -1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
 -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
 newton.array.io:8090  2>&1 | tee latency_r@1000ms_c12_gpon.log
 
-tcpkali -vvv --ws -c24 -r @2000ms \
+tcpkali -vv --ws -c24 -r @2000ms \
 --latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
 -1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
 -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
