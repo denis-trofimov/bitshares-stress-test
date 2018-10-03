bb3ee12abda00ee227e9beb95f13f1484dfbae1a
Denis Trofimov <silaradost@yandex.ru>
2018-10-03 21:43:39 +0300

    Add GPON home test results.

--- tcpkali/latency_limited_message_rate.sh
+++ tcpkali/latency_limited_message_rate.sh
@@ -10,3 +10,39 @@ tcpkali -vvv --ws -c100 -r @1000ms --connect-rate 10 \
 -1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
 -m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
 newton.array.io:8090  2>&1 | tee latency_r@1000ms_c100.log
+
+tcpkali -vvv --ws -c10 -r @1000ms \
+--latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
+-1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
+-m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
+newton.array.io:8090  2>&1 | tee latency_r@1000ms_c10_gpon.log
+
+tcpkali -vvv --ws -c2 -r @1000ms \
+--latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
+-1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
+-m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
+newton.array.io:8090  2>&1 | tee latency_r@1000ms_c2_gpon.log
+
+tcpkali -vvv --ws -c6 -r @1000ms \
+--latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
+-1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
+-m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
+newton.array.io:8090  2>&1 | tee latency_r@1000ms_c6_gpon.log
+
+tcpkali -vvv --ws -c24 -r @1000ms \
+--latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
+-1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
+-m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
+newton.array.io:8090  2>&1 | tee latency_r@1000ms_c24_gpon.log
+
+tcpkali -vvv --ws -c12 -r @1000ms \
+--latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
+-1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
+-m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
+newton.array.io:8090  2>&1 | tee latency_r@1000ms_c12_gpon.log
+
+tcpkali -vvv --ws -c24 -r @2000ms \
+--latency-marker "transaction_merkle_root" --message-stop "Assert Exception" \
+-1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
+-m '{"method": "call", "params": [2, "get_block", [\{connection.uid}]], "id": 7}' \
+newton.array.io:8090  2>&1 | tee latency_r@2000ms_c24_gpon.log
--- tcpkali/rate_test_message.sh
+++ tcpkali/rate_test_message.sh
@@ -13,3 +13,8 @@ tcpkali -T 180 -vvv -c12 -r400 --ws \
 -1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
 -m '{"method": "call", "params": [2, "get_block", [\{re [0-9]{1,3}}]], "id": 7}' \
 hawking.array.io:8090 2>&1 | tee rate400_c12_sec180.log
+
+tcpkali -T 60 -vvv -c6 -r1000 --ws \
+-1 '{"method": "call", "params": [1, "database", []], "id": 3}' \
+-m '{"method": "call", "params": [2, "get_block", [\{re [0-9]{1,3}}]], "id": 7}' \
+hawking.array.io:8090 2>&1 | tee rate1000_c6_gpon.log
